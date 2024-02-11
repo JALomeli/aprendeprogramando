19957b4ce6cb3b7895128fe9fcfd8d7e5eda3365
Jose Antonio Lomeli Garcia <josea.lomeligar.scch@gmail.com>
2024-02-10 22:23:35 -0600

    Pagina inico completada, header y footer finalizados

--- components/Footer.js
+++ components/Footer.js
@@ -4,34 +4,60 @@ import { Link } from "react-daisyui";
 
 export default function Footer() {
   return (
-    
-
-<footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
-    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-            </a>
-            <ul class="flex flex-wrap items-cnenter mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contact</a>
-                </li>
-            </ul>
-        </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+<footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4 ">
+  <div className="w-full max-w-screen-xl mx-auto p-4 md:py-3 flex flex-col md:flex-row md:justify-between items-center">
+    <div className="flex items-center mb-2 sm:mb-0 space-x-3 rtl:space-x-reverse">
+      <a href="https://flowbite.com/" className="hidden md:flex">
+        <img
+          src="Image/Logotipos/AprendeProgramandoComputadora.svg"
+          className="h-16 w-14"
+          alt="Flowbite Logo"
+        />
+      </a>
+      <a href="https://flowbite.com/" className="flex items-center md:hidden">
+        <img
+          src="Image/Logotipos/AprendeProgramadoNegroH.svg"
+          className="h-25 w-22"
+          alt="Flowbite Logo"
+        />
+      </a>
+      <a href="https://flowbite.com/" className="hover:underline hidden md:block">
+        Aprende Programando™
+      </a>
     </div>
+    <ul className="flex flex-col items-center  text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400 md:flex-row ">
+      <li>
+        <a href="#" className="hover:underline block md:inline-block  md:me-6">
+          Sobre nosotros
+        </a>
+      </li>
+      <li>
+        <a href="#" className="hover:underline block md:inline-block  md:me-6">
+          Política de privacidad
+        </a>
+      </li>
+      <li>
+        <a href="#" className="hover:underline block md:inline-block  md:me-6">
+          Licenciamiento
+        </a>
+      </li>
+      <li>
+        <a href="#" className="hover:underline block md:inline-block">
+          Contáctanos
+        </a>
+      </li>
+    </ul>
+  </div>
+  <hr className="my-4 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-5  " />
+  <span className=" text-center my-2 py-4 block text-sm text-gray-500 sm:text-center dark:text-gray-400 ">
+    © 2024{" "}
+    <a href="https://flowbite.com/" className="hover:underline   ">
+      Aprende Programando™
+    </a>
+    . Todos los derechos reservados.
+  </span>
 </footer>
+
 
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -73,14 +73,7 @@ export default function Header() {
                   Inicio
                 </a>
               </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-500 md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:border-gray-700 hover:bg-green-500"
-                >
-                  Sobre nosotros
-                </a>
-              </li>
+
               <li>
                 <a
                   href="#"
@@ -95,6 +88,22 @@ export default function Header() {
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-500 md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:border-gray-700 hover:bg-green-500"
                 >
                   Inicia Sesion
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-500 md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:border-gray-700 hover:bg-green-500"
+                >
+                  Examenes
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-500 md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:border-gray-700 hover:bg-green-500"
+                >
+                  Recursos Extra
                 </a>
               </li>
             </ul>
