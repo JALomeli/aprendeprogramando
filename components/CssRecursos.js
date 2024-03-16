fdc8d92e0a4678dbd58e782cbc7d32a51a22e158
Lomeli <josea.lomeligar.scch@gmail.com>
2024-03-15 23:44:23 -0600

    corregir titulos de html y css mal puestos

--- components/CssRecursos.js
+++ components/CssRecursos.js
@@ -7,7 +7,7 @@ export default function CssRecursos() {
     <main className="relative">
       <div className="flex flex-col bg-white justify-center items-center min-h-screen min-w-screen text-center m-5 p-5">
         <h1 className="text-4xl font-bold mb-4 mt-4">
-          Javascript Recursos adicionales
+          CSS Recursos adicionales
         </h1>
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
